17e4831d6a32f4f15d588c3057d8afc26d0f1cd1
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-08-17 21:19:34 +0100

    counters modified

---
+++ src/components/counter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+  };
+
+
+   //   Arrow functions do not rebind the this keyword. so to make this accesible to the
+  // handleIncrement function, we either use the arrow function or we use the
+  // constructor method as shown bellow.👇👇👇
+
+  //   constructor(){
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this)
+  //   }
+
+
+  handleIncrement = () => {
+    this.setState({value: this.state.value + 1});
+  }
+  render() {
+
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css';
+import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counters />
   </React.StrictMode>
 );
 
